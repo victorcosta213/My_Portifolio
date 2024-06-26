b3e17f14eab6a2e088aebdcc5f86569dd0e5218e
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-06-26 09:20:05 -0300

    feature: atualizando o hero

--- my-portifolio/src/pages/Home/Home.tsx
+++ my-portifolio/src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
+import Hero from "./sections/Hero/Hero"
+
 const Home = () => {
     return (
         <>
-        
+         <Hero/>
         </>
       )
     }
--- my-portifolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-portifolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import { Button, Container, Typography, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar.jpg"
+import {Grid} from "@mui/material"
+import DownloadIcon from '@mui/icons-material/Download';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Hero = () => {
+
+    const StyledHero = styled("div")(()=>({
+        backgroundColor: "black",
+        height:"100vh"
+
+    }))
+    const StyledImg = styled("img")(()=>({
+        width:"100%",
+        borderRadius: "50%"
+    }))
+
     return (
         <>
-            Hero
+           <StyledHero>
+                <Container maxWidth="lg">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={4}>
+                            <StyledImg src={Avatar}/>
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <Typography color="primary" variant="h1" textAlign="center">Victor Costa</Typography>
+                            <Typography color="primary" variant="h2" textAlign="center">I'm a software developer</Typography>
+
+                            <Grid container display="flex" justifyContent="center">
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <Button>
+                                       <DownloadIcon/>
+                                        Download CV
+                                    </Button>
+                                </Grid>
+                                <Grid item  xs={12} md={4} display="flex" justifyContent="center">
+                                    <Button>
+                                        <MailOutlineIcon/>
+                                        Contact me
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+          
+               
+           </StyledHero>
+        
+            
         </>
       )
     }
